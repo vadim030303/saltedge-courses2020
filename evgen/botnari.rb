abc8f158f84d84874266bda8dbf2b487164b0b35
Evgeny Botnari <evgenybotnari@gmail.com>
2020-02-25 20:38:31 +0200

    HW lesson4

---
+++ evgen/botnari.rb
@@ -0,0 +1,16 @@
+puts -123.odd?
+puts 6893.next
+puts 0.pred
+puts 12.to_f%4.5.round(2)
+puts (28%6)*4.7.round(-1)
+puts "Brand new bicycle\r\n".sub("\r\n", "")
+puts "Brand new bicycle".index("w")
+puts "Brand new bicycle".sub("bicycle", "")
+puts "Brand new bicycle".include?("bicycle")
+puts "Brand new bicycle"[6..8]
+puts "I thought I knew \neverything \nbefore that night"
+puts "Final result from Float part is:" + (-123.odd?).to_s
+puts "Final result from Float part is:" + (6893.next).to_s
+puts "Final result from Float part is:" + (12.to_f%4.5.round(2)).to_s
+puts "Final result from Float part is:" + ((28%6)*4.7.round(-1)).to_s
+puts "Final result from Float part is:" + (0.pred).to_s
